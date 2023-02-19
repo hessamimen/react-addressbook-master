5a1c93bb5c9444b9d012958713cfd5ef0561976c
Hessam Imen <hessam.imen@gmail.com>
2023-02-19 13:33:32 +1100

    chore: updated the previous file for functions

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// Function to fetch data from the server when submiting the Address form
+// Function to fetch data from the server when submitting the Address form.
 const BASE_URL = "http://localhost:3000";
 
 export const handleAddressSubmit = async (
@@ -20,4 +20,28 @@ export const handleAddressSubmit = async (
     setError(err.response.data.errormessage);
   }
   setLoading(false);
+};
+
+//Function to submit the person's name for each address
+
+export const handlePersonSubmit = (
+  addresses: any,
+  selectedAddress: string,
+  firstName: string,
+  lastName: string,
+  setError: React.Dispatch<React.SetStateAction<any>>,
+  addAddress: (arg0: any) => void
+) => {
+  if (!selectedAddress || !addresses.length) {
+    setError(
+      "No address selected, try to select an address or find one if you haven't"
+    );
+    return;
+  }
+
+  const foundAddress = addresses.find(
+    (address: any) => address.id === Number(selectedAddress)
+  );
+
+  addAddress({ ...foundAddress, firstName, lastName });
 };
