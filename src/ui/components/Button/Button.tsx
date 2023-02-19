1a9a6100700cf67d33222ab366df04526abc4a41
Hessam Imen <hessam.imen@gmail.com>
2023-02-19 13:50:35 +1100

    feat: implement conditional classname for the button component and styled based on variant

--- src/ui/components/Button/Button.tsx
+++ src/ui/components/Button/Button.tsx
@@ -1,19 +1,25 @@
 import React from "react";
-
+//Types
+import { ButtonProps } from "../../../types";
+//Styles
 import $ from "./Button.module.css";
+import cx from "classnames";
 
 const Button = ({
   children,
   onClick,
-  type = "button",
+  type,
   variant = "primary", // or 'secondary'
-}) => {
+}: ButtonProps) => {
   return (
     <button
       // TODO: Add conditional classNames
       // - Must have a condition to set the '.primary' className
       // - Must have a condition to set the '.secondary' className
-      className={$.button}
+      className={cx($.button, {
+        [$.primary]: variant === "primary",
+        [$.secondary]: variant === "secondary",
+      })}
       type={type}
       onClick={onClick}
     >
