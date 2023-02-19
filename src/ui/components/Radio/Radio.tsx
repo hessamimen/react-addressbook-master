1e4546345615c0242acf4382fc35fbb791024b0a
Hessam Imen <hessam.imen@gmail.com>
2023-02-19 13:55:20 +1100

    chore: The Radio component is converted to tsx

--- src/ui/components/Radio/Radio.tsx
+++ src/ui/components/Radio/Radio.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
 import $ from "./Radio.module.css";
+import { RadioProps } from "../../../types";
 
-const Radio = ({ children, id, name, onChange }) => {
+const Radio = ({ children, id, name, onChange }: RadioProps) => {
   return (
     <div className={$.radio}>
       <input type="radio" id={id} name={name} onChange={onChange} value={id} />
