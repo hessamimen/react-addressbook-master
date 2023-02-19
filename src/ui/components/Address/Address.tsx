d3c83ba35df608a52390fe0835dbecda643bec0f
Hessam Imen <hessam.imen@gmail.com>
2023-02-19 13:43:56 +1100

    chore: converted the Address component to tsx

--- src/ui/components/Address/Address.tsx
+++ src/ui/components/Address/Address.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-
+//Types
+import { AddressProps } from "../../../types";
+//Styles
 import $ from "./Address.module.css";
 
-const Address = ({ address }) => {
+const Address = ({ address }: AddressProps) => {
   return (
     <address className={$.address}>
       {address.street} {address.houseNumber}, {address.postcode}, {address.city}
